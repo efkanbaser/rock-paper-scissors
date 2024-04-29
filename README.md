b0d42c1e5890ae07a9a2b7f09caa5402cbad1167
efkanbaser <efkanbaser26@gmail.com>
2024-04-30 00:15:19 +0300

    Ui added with proper DOM manipulations.

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # rock-paper-scissors
 
 This is a consol based rock, paper and scissors game using JavaScript.
+
+Image taken from http://www.robotsvoice.com/janken/
--- app.js
+++ app.js
@@ -1,12 +1,31 @@
 let computerChoice
-let humanChoice
+let humanChoice = ""
 let computerScore = 0
 let humanScore = 0
+
+document.getElementById("rock").addEventListener("click", getHumanChoiceRock)
+document.getElementById("paper").addEventListener("click", getHumanChoicePaper)
+document.getElementById("scissors").addEventListener("click", getHumanChoiceScissors)
+
+function getHumanChoiceRock(){
+    humanChoice = "rock"
+    playGame()
+}
+
+function getHumanChoicePaper(){
+    humanChoice = "paper"
+    playGame()
+}
+
+function getHumanChoiceScissors(){
+    humanChoice = "scissors"
+    playGame()
+}
 
 function getComputerChoice(){
     computerChoice = ""
     let num = Math.random() 
-    if (num <= 0.3333333333333333333333333333) {
+    if (num <= 0.333333333333333333) {
         computerChoice = "rock"
     }
     else if (num >= 0.33333333333333333333333 && num <= 0.66666666666666666666666) {
@@ -18,63 +37,52 @@ function getComputerChoice(){
     return computerChoice
 }
 
-function getHumanChoice(){
-    humanChoice = ""
-    while (humanChoice !== "rock" && humanChoice !== "scissors" && humanChoice !== "paper"){
-    humanChoice = prompt("Rock, paper or scissors?").toLowerCase()
-    }
-    return humanChoice
-}
-
 function playRound(computerChoice, humanChoice){
     if (humanChoice == "rock" && computerChoice == "paper"){
-        console.log("You lose! Paper beats Rock.")
+        document.getElementById("display").innerText = "You lose! Paper beats Rock."
         computerScore += 1        
     }
     else if (humanChoice == "rock" && computerChoice == "scissors"){
-        console.log("You win! Rock beats Scissors.")        
+        document.getElementById("display").innerText = "You win! Rock beats Scissors."     
         humanScore += 1
     }
     else if (humanChoice == "paper" && computerChoice == "rock"){
-        console.log("You win! Paper beats Rock.")        
+        document.getElementById("display").innerText = "You win! Paper beats Rock."       
         humanScore += 1
     }
     else if (humanChoice == "paper" && computerChoice == "scissors"){
-        console.log("You lose! Scissors beats Paper.")        
+        document.getElementById("display").innerText = "You lose! Scissors beats Paper."       
         computerScore += 1
     }
     else if (humanChoice == "scissors" && computerChoice == "rock"){
-        console.log("You lose! Rock beats Scissors.")        
+        document.getElementById("display").innerText = "You lose! Rock beats Scissors."    
         computerScore += 1
     }
     else if (humanChoice == "scissors" && computerChoice == "paper"){
-        console.log("You win! Scissors beats Paper.")        
+        document.getElementById("display").innerText = "You win! Scissors beats Paper."      
         humanScore += 1
     }
     else {
-        console.log("Draw! Both of you chose the same option.")
+        document.getElementById("display").innerText = "Draw! Both of you chose the same option."
     }
-    console.log("Score: Human " + humanScore + " - " + computerScore + " Computer")
+    document.getElementById("score").innerText = "Score: Human " + humanScore + " - " + computerScore + " Computer"
+    document.getElementById("result").innerText = ""
 }
 
 function playGame(){
     let humanSelection, computerSelection
-    for (let i=0; i<5; i++){
-    humanSelection = getHumanChoice();
+    humanSelection = humanChoice;
     computerSelection = getComputerChoice();
     playRound(computerSelection, humanSelection);
+    if (humanScore == 5){
+        document.getElementById("result").innerText = "You have won the match!"
+        humanScore = 0
+        computerScore = 0
     }
-    if (humanScore > computerScore){
-        console.log("You have won the match!")
+    else if (computerScore == 5){
+        document.getElementById("result").innerText = "You have lost the match!"
+        humanScore = 0
+        computerScore = 0
     }
-    else if (humanScore < computerScore){
-        console.log("You have lost the match!")
-    }
-    else {
-        console.log("The match ends in a draw.")
-    }
-    humanScore = 0
-    computerScore = 0
 }
 
-playGame()
